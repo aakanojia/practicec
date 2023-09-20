c08f38e89aeccca89f09452414d7b089806b6651
aakanojia <aaryamann.kanojia@stonybrook.edu>
2023-09-20 17:41:26 -0400

    updated validargs and main

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -4,14 +4,41 @@
 #include "global.h"
 #include "debug.h"
 
+#define MAX_NODES 100 // Maximum number of nodes
+
+double matrix[MAX_NODES][MAX_NODES]; //Pre-declared 2D array to store the matrix
+
+int readAndValidateMatrix() {
+    int numNodes = 0; // To keep track of the number of nodes read.
+    char line[1024]; // Buffer to read each line
+}
+
 int main(int argc, char **argv)
 {
     if(validargs(argc, argv))
         USAGE(*argv, EXIT_FAILURE);
     if(global_options == HELP_OPTION)
         USAGE(*argv, EXIT_SUCCESS);
-    // TO BE IMPLEMENTED
-    return EXIT_FAILURE; 
+    // Check for MATRIX_OPTION
+    if(global_options & MATRIX_OPTION) {
+        // Implement the logic for the matrix mode
+        // Read from stdin, process, write to stdout
+    }
+
+    // Check for NEWICK_OPTION
+    if(global_options & NEWICK_OPTION) {
+        // Implement the logic for the Newick mode
+        // If outlier_name is set, you also have the `-o` flag provided
+        // Read from stdin, process, write to stdout
+    }
+
+    // If everything goes well
+    return EXIT_SUCCESS;
+
+    // If there's an error during processing, before the return statement above, you can:
+    // fprintf(stderr, "Error message here");
+    // return EXIT_FAILURE;
+    return EXIT_FAILURE;
 }
 
 /*
--- hw1/src/validargs.c
+++ hw1/src/validargs.c
@@ -3,6 +3,10 @@
 #include "global.h"
 #include "debug.h"
 
+// Compare string function prototype
+int compare(const char *str1, const char *str2);
+
+
 /**
  * @brief Validates command line arguments passed to the program.
  * @details This function will validate all the arguments passed to the
@@ -20,6 +24,51 @@
  */
 int validargs(int argc, char **argv)
 {
-    // TO BE IMPLEMENTED.
-    abort();
+    // Initialize global_options to 0
+    global_options = 0;
+
+    // If no arguments or only the program name is provided, return -1
+    if (argc == 1) { return -1;}
+
+    // Check for the -h flag
+    for (int i = 1; i < argc; i++) {
+        if (compare(argv[i], "-h") == 0) {
+            global_options |= HELP_OPTION;
+            return 0;
+        }
+    }
+
+    // Check for the -m and -n options
+    for (int i = 1; i < argc; i++) {
+        if (compare(argv[i], "-m") == 0) {
+            global_options |= MATRIX_OPTION;
+        } else if (compare(argv[i], "-n") == 0) {
+            global_options |= NEWICK_OPTION;
+            // If -n is followed by -o, store the outlier name
+            if (i + 2 < argc && compare(argv[i + 1], "-o") == 0) {
+                outlier_name = argv[i + 2];
+                i += 2; // Skip the next two arguments
+            }
+        }
+    }
+
+    // Check for invalid flag combinations
+    if ((global_options & MATRIX_OPTION) && (global_options & NEWICK_OPTION)) { return -1; }
+
+    // If neither MATRIX_OPTION nor NEWICK_OPTION is set, return -1
+    if (!(global_options & MATRIX_OPTION) && !(global_options & NEWICK_OPTION)) { return -1; }
+
+    // After parsing all flags, check if -o is provided without -n
+    if (outlier_name && !(global_options & NEWICK_OPTION)) { return -1; }
+
+    return 0;
+}
+
+// Custom string comparison and manipulation function
+int compare(const char *str1, const char *str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *(unsigned char *)str1 - *(unsigned char *)str2;
 }
